6f3771cb7a39afca3f8f141e870132040aa6ff08
enzonjagi <amosndungo@gmail.com>
2023-05-16 10:12:02 +0300

    Consuming RESTAPI Successfully

--- bookapp/views.py
+++ bookapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import requests
 
-# Create your views here.
+def users(request):
+    """We'll use this to pull data from the rest API"""
+
+    # pulling the data
+    response = requests.get(
+        'https://jsonplaceholder.typicode.com/users'
+    )
+
+    # let's convert the response into json
+    users = response.json()
+    # print(users)
+
+    return render(request, "users.html", {'users': users})
